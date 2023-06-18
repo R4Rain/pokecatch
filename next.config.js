3fe0047ebafae15740c5ddb0c559820e47378d2c
R4Rain <james.martin@binus.ac.id>
2023-06-19 05:39:39 +0700

    Change base path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/pokemon',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
